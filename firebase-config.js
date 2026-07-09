222f69da92ad51e50251de2b33487c675688a841
route-book <communication.route@scouts-europe.be>
2026-07-09 11:40:22 +0200

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,15 @@
+const FIREBASE_CONFIG = {
+  apiKey:            "AIzaSyB3f3dMpmt7NK-1b-L1cew0yalWToHGpQM",
+  authDomain:        "dossier-route.firebaseapp.com",
+  projectId:         "dossier-route",
+  databaseURL:       "https://dossier-route-default-rtdb.europe-west1.firebasedatabase.app",
+  storageBucket:     "dossier-route.firebasestorage.app",
+  messagingSenderId: "262600923353",
+  appId:             "1:262600923353:web:834858e6b372129b2b86c8"
+};
+
+const KNOWN_CLANS = [
+  { id: 'damiaan',  label: 'Clan Damiaan'  },
+  { id: 'godefroy', label: 'Clan Godefroy' },
+  { id: 'kolbe',    label: 'Clan Kolbe'    }
+];
